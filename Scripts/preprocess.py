9729a487b730cbd438e57057f549c51056bc81b8
abminara <abminara@gmail.com>
2014-01-11 20:00:31 -0500

    Preprocessing script
    Remove punctuation and lowercase the documents (except inside xml tags)

---
+++ Scripts/preprocess.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import codecs, os, sys, xml.dom.minidom as minidom
+path = r'C:\Users\Minas\Desktop\collateOS'
+
+def stripPunct(string):
+    punct = '!#$%&=\'()*+,-.:;?@[\\]^_`{|}~'
+    assemble = []
+    inTag= False
+    for char in string:
+        if char == '<':
+            inTag = True
+        elif char == '>':
+            inTag = False
+        if inTag:
+            assemble.append(char) #add char to the output unaltered, if it's inside an xml tag
+        elif not char in punct:
+            assemble.append(char.lower()) #if charis not part of tag and is not a punctuation mark, add char to the output lowercased
+    return ''.join(assemble)
+
+for f in filter(lambda x: str(x.split('.')[len(x.split('.'))-1]) == 'xml' , os.listdir(path)):
+    xmlDoc = stripPunct(codecs.open(os.path.join(path, f), 'r', encoding='UTF-8').read())
