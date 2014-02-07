9be09f52fcbbf6b6ee5f66b12dd0e746fa2dfb4b
David J Birnbaum <djbpitt@gmail.com>
2014-02-07 07:59:37 -0500

    Generate HTML view of CollateX JSON output, with CSS styling

--- Scripts/jsonToHTML.py
+++ Scripts/jsonToHTML.py
@@ -1,17 +1,17 @@
 import json
 data = json.loads(open('output.json', 'r').read())
 out = open('testOut.html', 'w')
-out.write('<html><head><title>test</title></head><body><table border="1", style="display: inline-block;">')
+out.write('<html xmlns="http://www.w3.org/1999/xhtml">\n<head>\n<title>test</title>\n<link href="http://www.obdurodon.org/css/style.css" rel="stylesheet" type="text/css"/>\n<link href="css/scholia.css" rel="stylesheet" type="text/css"/>\n<link href="css/collate.css" rel="stylesheet" type="text/css"/>\n<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8"/>\n</head>\n<body>\n<table>')
 for wName in data['witnesses']:
-    out.write('<tr><td>' + wName.encode('utf-8') + '</td></tr>')
-out.write('</table>')
+    out.write('\n<tr><td>' + wName.encode('utf-8') + '</td></tr>')
+out.write('\n</table>')
 for wordList in data['table']:
-    out.write('<table border="1", style="display: inline-block;">')
+    out.write('\n<table>')
     for word in wordList:
         if word:
-            out.write('<tr><td>' + word[0]['t'].encode('utf-8') + '</td></tr>')
+            out.write('\n<tr><td>' + word[0]['t'].encode('utf-8') + '</td></tr>')
         else:
-            out.write('<tr><td></td></tr>')
-    out.write('</table>')
-out.write('</body></html>')
+            out.write('\n<tr><td>&#xa0;</td></tr>')
+    out.write('\n</table>')
+out.write('\n</body>\n</html>')
 out.close()
