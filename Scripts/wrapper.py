2fe6c504df2541f6604abb0ef8376de857da9575
abminara <abminara@gmail.com>
2014-03-01 23:30:21 -0500

    Updated wrapper, should work on macs.

--- Scripts/wrapper.py
+++ Scripts/wrapper.py
@@ -9,34 +9,34 @@ print args
 if script.lower() == 'xmltojson.py':
     sameDir = True
     print 'Passing command: python xmltojson.py -i ' + In
-    subprocess.Popen('python xmltojson.py -i ' + In).wait()
+    subprocess.Popen(['python', 'xmltojson.py', '-i', In]).wait()
 elif script.lower() == 'xmlstojson.py':
     sameDir = False
     Out = args[args.index('-o')+1]
     print 'Passing command: python xmlstojson.py -i ' + In + ' -o ' + Out
-    subprocess.Popen('python xmlstojson.py -i ' + In + ' -o ' + Out).wait()
+    subprocess.Popen(['python', 'xmltojson.py', '-i', In, '-o', Out]).wait()
 else:
     raise Exception("Input script incorrect")
 if '-new' in args:
     if sameDir:
         print 'Passing command: python runCollatex.py -i ' + os.path.abspath(In) + ' -new'
-        subprocess.Popen('python runCollatex.py -i ' + os.path.abspath(In) + ' -new').wait()
+        subprocess.Popen(['python', 'runCollatex.py', '-i', os.path.abspath(In), '-new']).wait()
         print 'Passing command: python jsontoxml.py -i ' + os.path.join(os.path.abspath(In), 'collatexoutput')
-        subprocess.Popen('python jsontoxml.py -i ' + os.path.join(os.path.abspath(In), 'collatexoutput'))
+        subprocess.Popen(['python', 'jsontoxml.py', '-i', os.path.join(os.path.abspath(In), 'collatexoutput')]).wait()
     else:
         print 'Passing command: python runCollatex.py -i ' + os.path.abspath(os.path.dirname(Out)) + ' -new'
-        subprocess.Popen('python runCollatex.py -i ' + os.path.abspath(os.path.dirname(Out)) + ' -new').wait()
+        subprocess.Popen(['python', 'runCollatex.py', '-i', os.path.abspath(os.path.dirname(Out)), '-new']).wait()
         print 'Passing command: python jsontoxml.py -i ' + os.path.join(os.path.abspath(Out), 'collatexoutput')
-        subprocess.Popen('python jsontoxml.py -i ' + os.path.join(os.path.abspath(Out), 'collatexoutput'))
+        subprocess.Popen(['python', 'jsontoxml.py', '-i', os.path.join(os.path.abspath(Out), 'collatexoutput')]).wait()
 else:
     if sameDir:
         print 'Passing command: python runCollatex.py -i ' + os.path.abspath(In)
-        subprocess.Popen('python runCollatex.py -i ' + os.path.abspath(In)).wait()
+        subprocess.Popen(['python', 'runCollatex.py', '-i', os.path.abspath(In)]).wait()
         print 'Passing command: python jsontoxml.py -i ' + os.path.abspath(In)
-        subprocess.Popen('python jsontoxml.py -i ' + os.path.abspath(In))
+        subprocess.Popen(['python', 'jsontoxml.py', '-i', os.path.abspath(In)]).wait()
     else:
         print 'Passing command: python runCollatex.py -i ' + os.path.abspath(os.path.dirname(Out))
-        subprocess.Popen('python runCollatex.py -i ' + os.path.abspath(os.path.dirname(Out))).wait()
+        subprocess.Popen(['python', 'runCollatex.py', '-i', os.path.abspath(os.path.dirname(Out))]).wait()
         print 'Passing command: python jsontoxml.py -i ' + os.path.abspath(Out)
-        subprocess.Popen('python jsontoxml.py -i ' + os.path.abspath(Out))
+        subprocess.Popen(['python', 'jsontoxml.py', '-i', os.path.abspath(Out)]).wait()
 print 'Took', datetime.datetime.now()-startTimeWP, 'to execute wrapper.py'
